cdf9afb213ab9d84b45a947d634961a0c988bdc5
nottoday2471 <72010868+nottoday2471@users.noreply.github.com>
2022-05-09 22:12:32 +0300

    add "share with me"

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -4,8 +4,12 @@ import '../styles/account-styles.css';
 import Backendless from "backendless";
 import axios from 'axios';
 import fileDownload from "js-file-download";
+import ModalWindow from "./modal-window";
 
 function Account() {
+
+    const iconForNonImageFile = 'https://eu.backendlessappcontent.com/34B1584C-F24F-4764-FF20-7119C78FAD00/087FFB8B-902E-4389-85EF-83CB89874E7D/files/special_folder/file_icon.png'
+    const regExForFileName = /.(jpg|png|gif)$/i
 
     const [user, setUser] = useState({})
     const [file, setFile] = useState()
@@ -13,6 +17,8 @@ function Account() {
     const [avatarURL, setAvatarURL] = useState({})
     const [userFiles, setUserFiles] = useState([])
     const [fileURL, setFileURL] = useState('')
+    const [modalIsActive, setModalIsActive] = useState(false)
+    const [fileForShare, setFileForShare] = useState({})
 
     const navigate = useNavigate()
 
@@ -94,6 +100,19 @@ function Account() {
         }
     }
 
+    const fileNameHandler = fileName => {
+        const nameOfFile = fileName.split('').slice(0, 30)
+        nameOfFile.push('...')
+        return nameOfFile.join('')
+    }
+
+    const shareWith = (fileURL, fileName) => {
+        /*When clicked button "Share with" activate flag isOpen, then open modal component,
+        then find the user for share and create in them folder folder "Share with me" and add public URL into .txt file*/
+        setModalIsActive(true)
+        setFileForShare({fileURL, fileName})
+    }
+
     useEffect( () => {
         const fetchData = async () => {
             await getUser()
@@ -104,53 +123,57 @@ function Account() {
     })
 
     return(
-        <div className="account-container">
-            <div className="account-content">
-                {user ?
-                    <div className="">
-                        <div className="user-info">
-                            <div className="user-info-title">
-                                <p>{user.email}</p>
-                                <p>{user.country}</p>
-                            </div>
-                            <div className="user-info-avatar">
-                                <img src={avatar} alt="avatar" width="35px" height="35px"/>
-                                <input type="file" onChange={event => setAvatar(event.target.files[0])}/>
-                                <button onClick={uploadAvatar}>Choose avatar</button>
-                            </div>
-                        </div>
-                        <button onClick={LogOut} className="submit-logout">Log out</button><br />
-                        <div className="upload-file-form">
-                            <input type="file" onChange={event => setFile(event.target.files[0])}/>
-                            <button onClick={uploadFile}>Upload</button>
-                        </div>
-                        <div className="user-files">
-                            <button onClick={getUserFiles}>Get Files</button>
-                            {userFiles.length !== 0 ?
-                                <div className="user-file-list">
-                                    {userFiles.map((file, index) => {
-                                        return(
-                                            <div className="file-container" key={index}>
-                                                <p className="file-name">{file.name}</p>
-                                                <button className="download-button" onClick={() => downloadFile(file.name)}>Download</button>
-                                                <img src={file.publicUrl} alt="user-files" width="50px" height="50px"/>
-                                                <button className="download-button" onClick={() => deleteFile(file.publicUrl)}>X</button>
-                                            </div>
-                                        )
-                                    })}
+        <div>
+            {modalIsActive? <ModalWindow file={fileForShare}/>: <div></div>}
+            <div className="account-container">
+                <div className="account-content">
+                    {user ?
+                        <div className="">
+                            <div className="user-info">
+                                <div className="user-info-title">
+                                    <p>{user.email}</p>
+                                    <p>{user.country}</p>
                                 </div>
-                            :
-                                <div className="user-file-list">
-                                    <p>At that moment you don`t have any files</p>
+                                <div className="user-info-avatar">
+                                    <img src={avatar} alt="avatar" width="35px" height="35px"/>
+                                    <input type="file" onChange={event => setAvatar(event.target.files[0])}/>
+                                    <button onClick={uploadAvatar}>Choose avatar</button>
                                 </div>
-                            }
+                            </div>
+                            <button onClick={LogOut} className="submit-logout">Log out</button><br />
+                            <div className="upload-file-form">
+                                <input type="file" onChange={event => setFile(event.target.files[0])}/>
+                                <button onClick={uploadFile}>Upload</button>
+                            </div>
+                            <div className="user-files">
+                                <button onClick={getUserFiles}>Get Files</button>
+                                {userFiles.length !== 0 ?
+                                    <div className="user-file-list">
+                                        {userFiles.map((file, index) => {
+                                            return(
+                                                <div className="file-container" key={index}>
+                                                    <p className="file-name">{file.name.length > 30? fileNameHandler(file.name): file.name}</p>
+                                                    <button className="download-button" onClick={() => downloadFile(file.name)}>Download</button>
+                                                    <img src={regExForFileName.test(file.name)? file.publicUrl: iconForNonImageFile} alt="user-files" width="50px" height="50px"/>
+                                                    <button onClick={() => shareWith(file.publicUrl, file.name)}>Share with...</button>
+                                                    <button className="download-button" onClick={() => deleteFile(file.publicUrl)}>X</button>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                    :
+                                    <div className="user-file-list">
+                                        <p>At that moment you don`t have any files</p>
+                                    </div>
+                                }
+                            </div>
                         </div>
-                    </div>
-                :
-                    <div className="">
-                        <p>Log in please</p>
-                    </div>
-                }
+                        :
+                        <div className="">
+                            <p>Log in please</p>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     )
---
+++ src/components/modal-window.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+import '../styles/modal-styles.css'
+import Backendless from "backendless"
+import axios from "axios"
+
+function ModalWindow(props) {
+
+    const [user, setUser] = useState('')
+    const [userStatus, setUserStatus] = useState(false)
+
+    const getUserByName = async (username) => {
+        try {
+            const whereClause = `name = '${user}'`
+            const queryBuilder = await Backendless.DataQueryBuilder.create().setWhereClause(whereClause)
+            const [ userByName ] = await Backendless.Data.of('Users').find(queryBuilder)
+            if(userByName) {
+                setUserStatus(true)
+                try {
+                    console.log(props.file.fileURL)
+                    const res = await Backendless.Files.copyFile(props.file.fileURL,`https://eu.backendlessappcontent.com/34B1584C-F24F-4764-FF20-7119C78FAD00/087FFB8B-902E-4389-85EF-83CB89874E7D/files/${user}/shared_with_me`)
+                    // alert(`You are share with ${user}`)
+                    // const res = await axios.put('https://naturaltray.backendless.app/api/files/copy', {
+                    //     sourcePath : `props.file.fileURL`,
+                    //     targetPath: `https://eu.backendlessappcontent.com/34B1584C-F24F-4764-FF20-7119C78FAD00/087FFB8B-902E-4389-85EF-83CB89874E7D/files//${user}/shared_with_me`
+                    // })
+                    console.log(res)
+                } catch(err) {
+                    console.log(err)
+                }
+            } else {
+                setUserStatus(false)
+                return
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <div className="modal-container">
+            <div className="modal-content">
+                <p>File name - {props.file.fileName}</p>
+                <input
+                    type="text"
+                    value={user}
+                    onChange={event => setUser(event.target.value)}
+                />
+                <p className={userStatus? 'userExists': 'userDoesntExists'}>{userStatus? 'user exists': 'user doesnt exists'}</p>
+                <button className="submit-button" onClick={getUserByName}>Share</button>
+            </div>
+        </div>
+    )
+}
+
+export default ModalWindow
